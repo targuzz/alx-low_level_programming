4f62d943a7e2643ac4229d86d3618cd19122b918
targuzz <lihleliviwe@gmail.com>
2023-10-26 13:55:28 -0700

    Mandatory tasks

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,6 +3,7 @@
  * set_bit - set the bit of a number to 1
  * @n: pointer to the bit to be manipulated
  * @index: position of the bit to be manipulated
+ * Return: 1 on success and -1 failed
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
